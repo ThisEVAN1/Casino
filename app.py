bc7b1d4d72fd8848d8f56e082751c6b20b58e8d5
Evan Xu <evanxu365@gmail.com>
2025-06-01 19:42:34 -0700

    Added the login, logout, and register in the app.py file.

--- app.py
+++ app.py
@@ -32,10 +32,95 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 @app.route('/')
+@login_required
 def index():
     """ Show the menu """
     
     flash('Message')
     
     return apology('TODO')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """ Allow user to register for an account """
+    
+    # Checks if the user is already logged in
+    if 'user_id' in session:
+        return apology('Already logged in')
+
+    # If the user presses the register button
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirmation = request.form.get('confirmation')
+        
+        # Checks if the user stuff in the textfields
+        if not username or not password or not confirmation:
+            return apology('Missing textfields', 406)
+        
+        # Checks if the length of the username, password, and confirmation password is in the min and max range
+        if len(username) < 3 or len(username) > 20 or len(password) < 8 or len(password) > 200 or len(confirmation) < 8 or len(confirmation) > 200:
+            return apology("Please stop hacking I'm scared", 406)
+        
+        # Insert the user's username and hashed password into the database
+        db.execute('INSERT INTO users (username, hash) VALUES (?, ?)', username, generate_password_hash(password))
+        
+        # Log the user in
+        session['user_id'] = db.execute('SELECT id FROM users WHERE username = ?', username)[0]['id']
+        
+        return redirect('/')
+
+    return render_template('register.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+    
+    
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
